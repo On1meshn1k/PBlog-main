a32ce682f4cef7e31a27654af3617365e12f3994
On1meshn1k <pashenka.gorbunov.05@mail.ru>
2024-12-03 02:29:23 +0500

    database added

--- pblog/videohost/views.py
+++ pblog/videohost/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 def index(request): #HttpRequest
     return render(request, 'videohost/index.html')
 
-
 def enter(request):
     return render(request, 'videohost/enter.html')
 
